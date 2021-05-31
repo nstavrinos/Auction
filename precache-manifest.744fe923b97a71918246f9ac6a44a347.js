575d0bf03f2aad91927d1a5668778597aeef35ac
nstavrinos <n.stavrinos99@gmail.com>
2021-06-01 00:50:19 +0300

    Updates

---
+++ precache-manifest.744fe923b97a71918246f9ac6a44a347.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c69a4a116337699157273d8dcd9780",
+    "url": "/Auction/index.html"
+  },
+  {
+    "revision": "386975e1e44c809f9223",
+    "url": "/Auction/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5722e6d8baf612a63b7e",
+    "url": "/Auction/static/js/2.434aa09b.chunk.js"
+  },
+  {
+    "revision": "386975e1e44c809f9223",
+    "url": "/Auction/static/js/main.0e0a0527.chunk.js"
+  },
+  {
+    "revision": "09525da46a5127df0276",
+    "url": "/Auction/static/js/runtime-main.079c85fb.js"
+  }
+]);
